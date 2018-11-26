22ccf061bdea68ddce763d517a2f0e4a9f4e937f
hato375 <hato@yoods.co.jp>
2018-11-26 13:38:26 +0900

    １．初期登録

---
+++ include/rqt_param_manager/image_view.h
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2011, Dirk Thomas, TU Darmstadt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the TU Darmstadt nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef rqt_param_manager__ImageView_H
+#define rqt_param_manager__ImageView_H
+
+#include <rqt_gui_cpp/plugin.h>
+
+
+#include <ros/package.h>
+#include <ros/macros.h>
+#include <sensor_msgs/Image.h>
+#include <geometry_msgs/Point.h>
+
+#include <opencv2/core/core.hpp>
+
+#include <QAction>
+#include <QImage>
+#include <QList>
+#include <QString>
+#include <QSize>
+#include <QWidget>
+
+#include <vector>
+
+#include <ui_test.h>
+
+namespace rqt_param_manager {
+
+class ImageView
+  : public rqt_gui_cpp::Plugin
+{
+
+  Q_OBJECT
+
+public:
+
+  ImageView();
+
+  virtual void initPlugin(qt_gui_cpp::PluginContext& context);
+
+  virtual void shutdownPlugin();
+
+
+protected slots:
+
+
+protected:
+
+
+protected slots:
+
+
+protected:
+
+  Ui::Test ui_;
+
+  QWidget* widget_;
+
+private:
+};
+
+}
+
+#endif // rqt_param_manager__ImageView_H
